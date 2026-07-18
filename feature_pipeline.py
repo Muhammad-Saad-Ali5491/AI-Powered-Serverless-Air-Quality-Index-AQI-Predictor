e84d1998faa167a93fd88faad725f5c5be1be228
Muhammad-Saad-Ali5491 <saadali6809@gmail.com>
2026-07-19 00:51:49 +0500

    Day 2 hopsworks setup

--- feature_pipeline.py
+++ feature_pipeline.py
@@ -11,12 +11,12 @@ def fetch_raw():
     pollution = requests.get(
         f"http://api.openweathermap.org/data/2.5/air_pollution?lat={LAT}&lon={LON}&appid={API_KEY}"
     ).json()
-    print("POLLUTION RESPONSE:", pollution)  # temporary debug line
+   
 
     weather = requests.get(
         f"http://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
     ).json()
-    print("WEATHER RESPONSE:", weather)  # temporary debug line
+    
 
     return pollution, weather
 
